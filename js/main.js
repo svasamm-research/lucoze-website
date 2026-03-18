9c677e8ec5093d81e97c83d870874e4e869ee758
Mithun Kumar Singh <mithune0000@outlook.com>
2026-03-18 12:16:58 +0530

    style: fix prettier formatting in main.js

--- js/main.js
+++ js/main.js
@@ -358,8 +358,8 @@
 			var API_BASE = window.location.hostname.includes("localhost")
 				? "http://lucoze.admin.localhost:8000"
 				: window.location.hostname.includes("uat-website.lucoze.com")
-				? "https://admin-uat.lucoze.com"
-				: "https://admin.lucoze.com";
+					? "https://admin-uat.lucoze.com"
+					: "https://admin.lucoze.com";
 
 			fetch(API_BASE + "/api/method/lucoze_admin.api.provisioning.create_tenant", {
 				method: "POST",
@@ -500,8 +500,8 @@
 			var API_BASE = window.location.hostname.includes("localhost")
 				? "http://lucoze.admin.localhost:8000"
 				: window.location.hostname.includes("uat-website.lucoze.com")
-				? "https://admin-uat.lucoze.com"
-				: "https://admin.lucoze.com";
+					? "https://admin-uat.lucoze.com"
+					: "https://admin.lucoze.com";
 
 			fetch(API_BASE + "/api/method/lucoze_admin.api.contact.submit_contact", {
 				method: "POST",
